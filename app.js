2e48d79fed5006bdd3afb13d0f94a01fe6944042
Kari Tinsley <karitinsley57@gmail.com>
2023-01-24 15:41:51 -0700

    rearranged order of conditions

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ let target = Math.floor(Math.random*(maxNum - minNum)+minNum);
 
 input = Number(prompt("Guess a number between 1 and 10", ""));
 
-if (input === target){
-    window.alert("Correct!");
-}if(input !== target){
+if(input !== target){
 }else if(input < target){
     window.alert("Higher");
     prompt("Guess again");
 }else if (input > target){
     window.alert("Lower");
     prompt("Guess again");
+}else if (input === target){
+    window.alert("Correct!");
 }
 }
 
