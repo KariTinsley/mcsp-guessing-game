6fcfce85dda945662cdcb2107435afa1a20947a0
Kari Tinsley <karitinsley57@gmail.com>
2023-01-24 15:35:21 -0700

    tweaked prompt to give extra empty string for user input

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let target = Math.floor(Math.random*(maxNum - minNum)+minNum);
 let minNum = Math.ceil(1);
 let maxNum = Math.floor(10);
 
-input = Number(prompt("Guess a number between 1 and 10"));
+input = Number(prompt("Guess a number between 1 and 10", ""));
 
 if (input === target){
     window.alert("Correct!");
