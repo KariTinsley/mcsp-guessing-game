27844c82d4e64500841903438aff76506cf93836
Kari Tinsley <karitinsley57@gmail.com>
2023-01-24 16:27:49 -0700

    made more functions and reordered things

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-function guessOnce(input){
-let min = Math.ceil(1);
-let max = Math.floor(10);
-let target = Math.floor(Math.random()*(max - min + 1) + min); 
 
-input = Number(window.prompt("Guess a number between 1 and 10", ""));
+function randomNumber (min, max){
+    let min = Math.ceil();
+    let max = Math.floor();
+    return Math.floor(Math.random()*(max - min + 1) + min); 
+    }
 
-while(input !== target){
-    }if(input < target){
-    window.alert("Higher");
-    window.prompt("Guess again");
-    }else if (input > target){
-    window.alert("Lower");
-    window.prompt("Guess again");
-    }
-if (input === target){
-        window.alert("Correct!");
-    }
+function userGuesses(){
+    let input = Number(prompt("Guess a number between 1 and 10"));
+    return input;
 }
 
-
-console.log(guessOnce());
+function guessOnce(){
+    let target = randomNumber();
+        while(input !== target){
+            }if(input < target){
+                alert("Higher");
+                prompt("Guess again");
+            }else if (input > target){
+                alert("Lower");
+                prompt("Guess again");
+            }else {
+                alert("Correct!");
+        }
+}
